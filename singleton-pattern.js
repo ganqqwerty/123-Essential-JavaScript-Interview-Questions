c16af223b32200b4fb52faa90d3c1f227c506646
Nishant Kumar <nihantanu@gmail.com>
2015-06-22 10:38:07 +0530

    Added sigleton patter code example

--- singleton-pattern.js
+++ singleton-pattern.js
@@ -3,10 +3,47 @@ var emailClient = (function(){
 
 	// Instance Variable
 	var _instance;
-
+	
 	function init(){
-		
-	}
+
+		// Validate email address
+		function isEmailValid (emailAdd){
+			if(emailAdd){
+				return true
+			}else{
+				return false;
+			}
+		}
+
+		// Validate email configuration
+		function isValidConfig(objEmailConfi){
+			if(objEmailConfi){
+				return true;
+			}else{
+				return false;
+			}
+		}
+
+		return {
+			// Expose method which we want to use through well defined known point of access
+			sendEmail : function(objConfig){
+				if(isValidConfig(objConfig)){
+					console.log("Email has been sent to recipient successfully at : " + objConfig.toEmail);
+				}else{
+					console.log("Invalid email configuration");
+				}
+				
+			},
+
+			validateEmail : function(email){
+				if(isEmailValid(email)){
+					console.log("Provided email address is valid");
+				}else{
+					console.log("Provided email address is not valid");
+				}
+			}
+		}
+	} 
 
     // return single instance
 	return {
@@ -17,4 +54,22 @@ var emailClient = (function(){
 			return _instance;
 		}
 	}
-})
+})();
+
+
+// emailClient instance for sending email
+var googleEmailClient = emailClient.getInstance();
+    googleEmailClient.sendEmail({toEmail:'nihantanu2010@gmail.com', 
+								 fromEmail: 'callmefool@outlook.com',
+								 subject: 'greeting'
+								});
+
+// emailClient instance for validating email
+var validate = emailClient.getInstance();
+	validate.validateEmail('nihantanu2010@gmail.com');
+
+// check is both instance are similar or not 
+console.log(googleEmailClient === validate); // output true
+
+
+
