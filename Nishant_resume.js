d7bca626e507a50199fb529e5f146f2b8418aafa
Nishant Kumar <nihantanu@gmail.com>
2015-09-19 23:30:14 +0530

    Update Nishant_resume.js

--- Nishant_resume.js
+++ Nishant_resume.js
@@ -64,46 +64,46 @@
 	
 	/**
 	 *  Nishant is a very smart and thoughtful person as an engineer. He truly cares
-	 *	about helping others and values the learning experience for young software
-	 *	engineers. He is very patient - a key trait as an engineer. Nishant will help
-	 *	guide you through problems and help you understand software engineering
-	 *	concepts. Hands down I would hire Nishant to be on a team that will not only
-	 *	help a business but help other engineers grow as well.
-	 *		-Gary Tsai, Software Engineer
+	 *  about helping others and values the learning experience for young software
+	 *  engineers. He is very patient - a key trait as an engineer. Nishant will help
+	 *  guide you through problems and help you understand software engineering
+	 *  concepts. Hands down I would hire Nishant to be on a team that will not only
+	 *  help a business but help other engineers grow as well.
+	 *	-Gary Tsai, Software Engineer
 	 */
 	
 	/**
 	 *  Nishant is a quick learner and he has very deep understanding on UI stack. He
-	 *	also is a good mentor and help other to understand the technology stack. One
-	 *	good thing I observed while working with him is he always focus on insist on
-	 *	highest standard in his code/solution as well as idea. Keep it up. 
+	 *  also is a good mentor and help other to understand the technology stack. One
+	 *  good thing I observed while working with him is he always focus on insist on
+	 *  highest standard in his code/solution as well as idea. Keep it up. 
 	 *  	-Apurv Gupta (WDE II at Amazon)
 	 */
 	
 	 /**
 	  * Nishant is one of the best person I'm working with. Along with his strong
-	  *	technical understanding of how things work, he also keeps on motivating to team
+	  * technical understanding of how things work, he also keeps on motivating to team
 	  * and helps them to participate effectively & maintaining their productivity to
-	  *	better work towards achieving a goal.  He always prefers to have full stack
-	  *	knowledge of the product & leads a team with great enthusiasm. He can deliver
-	  *	end to end any product he is working on. 
-	  *	    -Yash Shah (SRE at Linkedin)
+	  * better work towards achieving a goal.  He always prefers to have full stack
+	  * knowledge of the product & leads a team with great enthusiasm. He can deliver
+	  * end to end any product he is working on. 
+	  *	 -Yash Shah (SRE at Linkedin)
 	  */
 	 
 	 /**
 	  * I have been working with Nishant since last one year on a cloud-based product,
-	  *	He is one of the vibrant contributor in the team and always been dedicated to
-	  *	his work. He has very good level of knowledge when it comes to overall front-end
-	  *	architecture solutions and specifically on frameworks like backbone & angular.
+	  * He is one of the vibrant contributor in the team and always been dedicated to
+	  * his work. He has very good level of knowledge when it comes to overall front-end
+	  * architecture solutions and specifically on frameworks like backbone & angular.
 	  *     -Mohiuddin Parekh ( Founder & CEO at Decojent)
 	  */
 	 
 	 /**
 	  * I first met Nishant 3 months ago when I told him I was looking for help with
 	  * AngularJS and asked him to mentor me. One of the first things I noticed was that
-      * he had a great learning attitude, because he told me that he was looking forward
-      * to learning from me, something I wasn’t expecting from a mentor! He is fun,
-      * sweet, generous and has a great sense of humor. We have quickly become friends
+          * he had a great learning attitude, because he told me that he was looking forward
+          * to learning from me, something I wasn’t expecting from a mentor! He is fun,
+          * sweet, generous and has a great sense of humor. We have quickly become friends
 	  * and I think we have spent more time hanging out on Telegram (instant messaging)
 	  * and Skype than we have coding. I had never written my own AngularJS directive
  	  * before so I asked him if he could write one for me for an autocomplete feature.
@@ -111,7 +111,7 @@
 	  * directives. We have spent a lot of time chatting and I think there is so much
 	  * more he can do that I have not yet seen. I can see he also has a passion for
 	  * startups. 
-	  * -Jenny Swift (Software Engineer) 
+	  *     -Jenny Swift (Software Engineer) 
 	  */
 
 })();
