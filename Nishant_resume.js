16454c8c6fcdb78b2cb58c1e9909f53cb3eb9880
Nishant Kumar <nihantanu@gmail.com>
2015-09-19 23:28:31 +0530

    Added my Resume
    Added my Resume

---
+++ Nishant_resume.js
@@ -0,0 +1,117 @@
+/**
+ *  The Curriculum vitae of Nishant Kumar
+ *
+ *  Hi there, I am Nishant. I am UI Engineering Lead, JavaScript Engineer,
+ *  Mozila Tech Contributor, CodeMentor Front-end Expert, Writer from India.
+ */
+
+(function() {
+	'use strict';
+
+	/*
+  	 About Me 
+ 	*/
+	var me = {
+		name: 'Nishant Kumar',
+		job: 'UI Engineering Lead',
+		organization: 'Big Parser',
+		status: 'Not availble for Hire full time',
+		location: 'India',
+		experience: '4 years 2 month',
+		skills: {
+			'scripting-language' :['javascript']
+			'frameworks': ['angular.js','backbone.js','sails.js'],
+			'library' : ['jQuery']
+			'tools': ['Gulp', 'Grunt', 'bower','yoman','Git'],
+			'testing': ['jasmine', 'protractor'],
+			'backend': ['node.js', 'express.js'],
+			'dataBase': ['mongoDb', 'SQL'],
+			'uncategorized' : ['C','C#','HTML','CSS', 'HTML5','JSON']
+		}
+	};
+
+	/*
+	  Contact Details
+	 */
+	var me_contact = {
+		'email' : 'nihantanu@gmail.com',
+		'skypeId' : 'mfsi_nishantk',
+		'linkedIn' :'http://in.linkedin.com/in/nishantkumar1'
+		'gitHub' : 'github.com/nishant8BITS'
+		'stackOverFlow' : 'http://stackoverflow.com/users/430803/nishant'
+		'codementor': 'codementor.io/nihantanu'
+	}
+
+	/*
+	   Education 
+	 */
+	var education = {
+		'post-Graduate': {
+			'college': 'Birla Institute of Technology and Science, Pilani',
+			'Major' : 'Software System',
+			'Year' :'2016'
+		},
+		'graduation' : {
+			'college': 'ICFAI University, Dehradun',
+			'Major' : 'Computer Science & Engineering',
+			'Year' :'2011'
+		}
+	}
+
+	/**
+	 *  Nice things people have said about me.
+	 */
+	
+	/**
+	 *  Nishant is a very smart and thoughtful person as an engineer. He truly cares
+	 *	about helping others and values the learning experience for young software
+	 *	engineers. He is very patient - a key trait as an engineer. Nishant will help
+	 *	guide you through problems and help you understand software engineering
+	 *	concepts. Hands down I would hire Nishant to be on a team that will not only
+	 *	help a business but help other engineers grow as well.
+	 *		-Gary Tsai, Software Engineer
+	 */
+	
+	/**
+	 *  Nishant is a quick learner and he has very deep understanding on UI stack. He
+	 *	also is a good mentor and help other to understand the technology stack. One
+	 *	good thing I observed while working with him is he always focus on insist on
+	 *	highest standard in his code/solution as well as idea. Keep it up. 
+	 *  	-Apurv Gupta (WDE II at Amazon)
+	 */
+	
+	 /**
+	  * Nishant is one of the best person I'm working with. Along with his strong
+	  *	technical understanding of how things work, he also keeps on motivating to team
+	  * and helps them to participate effectively & maintaining their productivity to
+	  *	better work towards achieving a goal.  He always prefers to have full stack
+	  *	knowledge of the product & leads a team with great enthusiasm. He can deliver
+	  *	end to end any product he is working on. 
+	  *	    -Yash Shah (SRE at Linkedin)
+	  */
+	 
+	 /**
+	  * I have been working with Nishant since last one year on a cloud-based product,
+	  *	He is one of the vibrant contributor in the team and always been dedicated to
+	  *	his work. He has very good level of knowledge when it comes to overall front-end
+	  *	architecture solutions and specifically on frameworks like backbone & angular.
+	  *     -Mohiuddin Parekh ( Founder & CEO at Decojent)
+	  */
+	 
+	 /**
+	  * I first met Nishant 3 months ago when I told him I was looking for help with
+	  * AngularJS and asked him to mentor me. One of the first things I noticed was that
+      * he had a great learning attitude, because he told me that he was looking forward
+      * to learning from me, something I wasn’t expecting from a mentor! He is fun,
+      * sweet, generous and has a great sense of humor. We have quickly become friends
+	  * and I think we have spent more time hanging out on Telegram (instant messaging)
+	  * and Skype than we have coding. I had never written my own AngularJS directive
+ 	  * before so I asked him if he could write one for me for an autocomplete feature.
+	  * I was impressed with his work and because of it I was then able to write my own
+	  * directives. We have spent a lot of time chatting and I think there is so much
+	  * more he can do that I have not yet seen. I can see he also has a passion for
+	  * startups. 
+	  * -Jenny Swift (Software Engineer) 
+	  */
+
+})();
